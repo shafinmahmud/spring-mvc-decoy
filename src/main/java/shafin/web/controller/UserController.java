a85053343c88ca8a8112abe1ba890011e57cf4c1
Shafin Mahmud <shafin.mahmud@gmail.com>
2016-12-11 01:44:42 +0600

    db transaction success

--- src/main/java/shafin/web/controller/UserController.java
+++ src/main/java/shafin/web/controller/UserController.java
@@ -1,9 +1,14 @@
 package shafin.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import shafin.web.entity.User;
+import shafin.web.service.UserService;
+
+import java.util.List;
 
 /**
  * @author SHAFIN
@@ -12,9 +17,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showHome(Model model) {
-        System.out.printf("OKK");
+
+        List<User> allUsers = userService.findAllUser();
+        model.addAttribute("userList", allUsers);
+
+        User user = new User("Shafin", "Dhaka");
+        userService.insertUser(user);
+
         return "home";
     }
 }
---
+++ src/main/java/shafin/web/service/UserService.java
@@ -0,0 +1,33 @@
+package shafin.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import shafin.web.dao.UserDao;
+import shafin.web.entity.User;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * @author SHAFIN
+ * @since 12/11/2016
+ */
+@Service(value = "userService")
+@Transactional
+public class UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    public List<User> findAllUser() {
+
+        return userDao.findAll();
+    }
+
+    public void insertUser(User user) {
+
+        if (!userDao.doesExist(user.getName())) {
+            userDao.saveOrUpdate(user);
+        }
+    }
+}
